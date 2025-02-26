996f69c35fdc22b8f6808a3c33941953a5d3f109
programmer-git-X <156421821+programmer-git-X@users.noreply.github.com>
2025-02-26 17:43:42 +0300

    Models Menu

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(
+        max_length=20, blank=False, null=False, verbose_name="Название раздела"
+    )
+    slug = models.SlugField(max_length=20, blank=False, null=False, verbose_name="URL")
+
+    class Meta:
+        db_table = "menu"
+        verbose_name = "Меню"
+        verbose_name_plural = "Меню"
+
+    def __str__(self):
+        return str(self.pk) + " - " + self.title
