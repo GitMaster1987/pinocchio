a147899a7bc53a39900f7ef547f665773f59485d
programmer-git-X <156421821+programmer-git-X@users.noreply.github.com>
2025-02-27 11:19:43 +0300

    title

--- main/views.py
+++ main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "main/base.html")
+    context = {
+        "title": "Пиноккио - Пицца, которая не оставит вас равнодушным",
+    }
+    return render(request, "main/index.html", context)
