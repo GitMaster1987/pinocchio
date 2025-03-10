00653a549d7694457d7d04dc0cc8b4205f1b56bf
programmer-git-X <156421821+programmer-git-X@users.noreply.github.com>
2025-03-10 17:22:59 +0300

    commit

--- users/views.py
+++ users/views.py
@@ -1,40 +1,11 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib import auth
-from django.http import HttpResponseRedirect
-from django.shortcuts import redirect, render
-from django.urls import reverse
-from users.forms import UserLoginForm
+from django.shortcuts import render
 
 # Create your views here.
 # Авторизация пользователя
 def login(request):
-    
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                 # Получаем URL, на который пользователь пытался зайти (если он есть)
-                next_url = request.GET.get('next')
-            # Если URL есть, перенаправляем на него, иначе на страницу journal:index
-                if next_url:
-                    return HttpResponseRedirect(reverse(next_url))
-                else:
-                    return HttpResponseRedirect(reverse('main:index'))
-    else:
-        if request.user.is_authenticated:
-           return redirect(reverse('main:index'))
-        else: 
-            form = UserLoginForm()
-    
-    context = {
-        'form' : form,
-    }
+   
 
-    return render(request, 'users/login.html', context)
+    return render(request, 'users/login.html')
 
 # Регистрация пользователя
 def register(request):
@@ -42,7 +13,6 @@ def register(request):
     return render(request, 'users/registration.html')
 
 # Выход
-@login_required
 def logout(request):
-    auth.logout(request)
-    return redirect(reverse('main:index'))
+   
+    return render(request, 'users/registration.html')
