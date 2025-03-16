c6965e99dc63a96fc04cec1f406bb3bc9cf5fb32
programmer-git-X <156421821+programmer-git-X@users.noreply.github.com>
2025-03-16 17:38:23 +0300

    Add to cart view, refactor included cart teamplates

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+from main.models import Products
+from users.models import User
+
+
+#  ''' Считаем общее кол-во товаров и их стоимость '''
+class CartQuerySet(models.QuerySet):
+    # Полная стоимость всех корзин покупателя
+    def total_price(self):
+        return sum(cart.products_price() for cart in self)
+
+    # Общее кол-во товаров в корзинах
+    def total_quantity(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+        return 0
+
+
+# Модель корзины товаров
+class Cart(models.Model):
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name="Пользователь",
+    )
+    session_key = models.CharField(
+        max_length=32, blank=True, null=True, verbose_name="Ключ сессии"
+    )
+    product = models.ForeignKey(
+        to=Products, on_delete=models.CASCADE, verbose_name="Продукт"
+    )
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name="Количество")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")
+
+    class Meta:
+        db_table = "carts"
+        verbose_name = "Корзину"
+        verbose_name_plural = "Корзины"
+
+    objects = CartQuerySet().as_manager()
+
+    # Цена общего кол-ва товаров
+    def products_price(self):
+        return round(self.product.sell_price() * self.quantity, 2)
+
+    def __str__(self):
+        return f"Корзина: {self.user.username} | Товар: {self.product.name} | Кол-во: {self.quantity}"
--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from carts.models import Cart
+from main.models import Products
 
 
 # Добавление в корзину
-def cart_add(request, product_id): ...
+def cart_add(request, product_id): 
+    product = Products.objects.get(id = product_id)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user = request.user, product = product)
+        # Если товар уже добавлен увеличиваем к-во на 1
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user = request.user, product=product, quantity = 1)
+    
+    return redirect(request.META['HTTP_REFERER'])
 
 
 # Изменение корзины
