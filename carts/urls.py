34a448b498f333f68481399a29087cbc3c02595f
programmer-git-X <156421821+programmer-git-X@users.noreply.github.com>
2025-03-19 10:38:47 +0300

    Cart removed controller added, commit befor ajax

--- carts/urls.py
+++ carts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # ''' CARTS '''
     path("cart_add/<int:product_id>/", views.cart_add, name="cart_add"),
     path("cart_change/<int:product_id>/", views.cart_change, name="cart_change"),
-    path("cart_remove/<int:product_id>/", views.cart_remove, name="cart_remove"),
+    path("cart_remove/<int:cart_id>/", views.cart_remove, name="cart_remove"),
 ]
