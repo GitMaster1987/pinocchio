c368b985ca668d2680666a9b524bebf1d8fc7ce4
programmer-git-X <156421821+programmer-git-X@users.noreply.github.com>
2025-04-13 10:39:21 +0300

    mysql databases

--- __pinocchio/settings.py
+++ __pinocchio/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,14 +81,25 @@ WSGI_APPLICATION = '__pinocchio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+''' 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+'''
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pinicchio',        # Имя вашей базы данных
+        'USER': 'admin',             # Имя пользователя MySQL
+        'PASSWORD': '//Ljwtyrj1027',  # Пароль пользователя
+        'HOST': 'localhost',            # Хост базы данных (например, localhost)
+        'PORT': '3306',                 # Порт MySQL (по умолчанию 3306)
     }
 }
-
+pymysql.install_as_MySQLdb()
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
